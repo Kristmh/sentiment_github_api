f28ec1a5b70c6b6f713ca103576f26306c3d1974
Kristian Marison Haugerud <haugerud.kristian@gmail.com>
2023-11-10 13:30:12 +0100

    feat: add sentiment and emotion analysis

--- fetch_github.py
+++ fetch_github.py
@@ -1,11 +1,14 @@
 import requests
 from dotenv import load_dotenv
 import os
+import spacy
+import re
 
 load_dotenv()
 personal_access_token = os.getenv("GITHUB_TOKEN")
 
-def fetch_github_issue_bodies(owner, repo, personal_access_token=None, max_issues=10):
+# 100 issues per page so adjust max pages accordingly
+def fetch_github_issues(owner, repo, personal_access_token=personal_access_token, max_pages=1):
     base_url = f"https://api.github.com/repos/{owner}/{repo}/issues"
     headers = {
         "Accept": "application/vnd.github.v3+json"
@@ -22,7 +25,7 @@ def fetch_github_issue_bodies(owner, repo, personal_access_token=None, max_issue
     page = 1
 
     while True:
-        if max_issues is not None and len(issue_bodies) >= max_issues:
+        if max_pages is not None and page > max_pages:
             break
         params["page"] = page
         response = requests.get(base_url, headers=headers, params=params)
@@ -31,18 +34,47 @@ def fetch_github_issue_bodies(owner, repo, personal_access_token=None, max_issue
         if not page_issues:
             break
         for issue in page_issues:
-            issue_bodies.append(issue["body"])
+            issue_body = issue["body"]
+            if issue_body:
+                issue_body_clean = clean_text(issue_body)
+                issue_bodies.append(issue_body_clean)
         page += 1
 
     return issue_bodies
 
-# Usage
-owner = "octocat"  # Replace with the desired username
-repo = "Hello-World"  # Replace with the desired repo name
-issue_bodies = fetch_github_issue_bodies(owner, repo, personal_access_token)
+# Load the small English model
+nlp = spacy.load("en_core_web_sm")
+def clean_text_spacy(text):
+    doc = nlp(text)
+    clean_tokens = [token.lemma_ for token in doc if not token.is_stop and not token.is_punct and not token.is_space and not token.like_url and not token.like_num]
+    return ' '.join(clean_tokens)
 
-print(issue_bodies)
-# Print the bodies of the fetched issues
-for body in issue_bodies:
-    print(body)
+def clean_text(text):
+    # Remove URLs
+    text = re.sub(r'http\S+', '[URL]', text)
+
+    # Remove HTML tags
+    text = re.sub(r'<.*?>', '', text)
+
+    # Remove special characters and numbers
+    text = re.sub(r'[^a-zA-Z\s]', '', text)
+
+    # Convert to lowercase
+    text = text.lower()
+
+    # Remove excessive whitespace
+    text = re.sub(r'\s+', ' ', text).strip()
+
+    return text
+
+if __name__ == "__main__":
+    # Usage
+    owner = "octocat"  # Replace with the desired username
+    repo = "Hello-World"  # Replace with the desired repo name
+    issue_bodies = fetch_github_issues(owner, repo, personal_access_token)
+
+    print(issue_bodies)
+    # Print the bodies of the fetched issues
+    for body in issue_bodies:
+        print(body)
 
---
+++ sentiment_analysis.py
@@ -0,0 +1,73 @@
+from numpy import empty
+from transformers import pipeline, AutoTokenizer
+from fetch_github import fetch_github_issues
+
+
+def predict_emotions(text: list):
+    emotions= []
+
+    model="SamLowe/roberta-base-go_emotions"
+    pipe = pipeline("text-classification", model=model)
+
+    tokenizer = AutoTokenizer.from_pretrained(model)
+
+    # Get the maximum length the model can handle
+    max_length = tokenizer.model_max_length
+
+    for issue in text:
+        if issue:
+            # Tokenize and truncate the text
+            tokens = tokenizer.encode(issue, add_special_tokens=True, truncation=True, max_length=max_length)
+
+            # Convert the tokens back to text string
+            truncated_text = tokenizer.decode(tokens, skip_special_tokens=True)
+
+            # Pass the truncated text to the pipeline
+            sentiment = pipe(truncated_text)
+            print(sentiment)
+            emotions.append({"issue": issue, "sentiment":sentiment})
+    return emotions
+
+def predict_sentiment(text: list):
+    sentiments = []
+    positive_issues = 0
+    negative_issues = 0
+    model = "distilbert-base-uncased-finetuned-sst-2-english"
+    sentiment_pipeline = pipeline("sentiment-analysis", model=model)
+    # Use a pipeline as a high-level helper
+
+    tokenizer = AutoTokenizer.from_pretrained(model)
+
+    # Get the maximum length the model can handle
+    max_length = tokenizer.model_max_length
+
+    for issue in text:
+        if issue:
+
+            # Tokenize and truncate the text
+            tokens = tokenizer.encode(issue, add_special_tokens=True, truncation=True, max_length=max_length)
+
+            # Convert the tokens back to text string
+            truncated_text = tokenizer.decode(tokens, skip_special_tokens=True)
+
+            # Pass the truncated text to the pipeline
+            sentiment = sentiment_pipeline(truncated_text)
+            print(sentiment)
+            sentiments.append({"issue": issue, "sentiment":sentiment})
+            results = sentiment[0]
+            if results["label"]== "POSITIVE":
+                positive_issues += 1
+            elif results["label"]== "NEGATIVE":
+                negative_issues += 1
+    return sentiments, positive_issues, negative_issues
+
+
+if __name__ == "__main__":
+    data = fetch_github_issues("microsoft", "vscode")
+    emotions = predict_emotions(data)
+    print(emotions)
+    sent, pos, neg = predict_sentiment(data)
+    #print(sent)
+    total = pos + neg
+    print(f"Positive issues: {pos} out of {total}")
+    print(f"Negative issues: {neg} out of {total}")
